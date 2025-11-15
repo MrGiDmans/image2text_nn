7ac8ec67c5f51e335e3d63ee936c016892b1e109
MrGiDmans <tumambae@gmail.com>
2025-11-15 15:25:36 +0500

    Первая инициализация

---
+++ src/utils/dataLoader.py
@@ -0,0 +1,9 @@
+import os
+
+class DataLoader:
+    def __init__(self, data_path):
+        self.data_path = data_path
+
+    def load_data(self):
+        # Implement data loading logic here
+        pass
